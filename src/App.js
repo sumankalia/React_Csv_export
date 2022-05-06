ae0604b70b474d59a3b57847d397a7775340cf46
Suman Kumar <suman.kalia235@gmail.com>
2022-05-07 03:31:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import { CSVLink } from "react-csv";
 
-function App() {
+const headers = [
+  { label: "Name", key: "name" },
+  { label: "Age", key: "age" },
+  { label: "Phone", key: "phone" },
+  { label: "Email", key: "email" },
+];
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [downloadedData, setDownloadedData] = useState([]);
+  const csvDownloadRef = useRef(0);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchDataToDownload = () => {
+    axios
+      .get("http://localhost:4000/users/get")
+      .then(({ data }) => {
+        setDownloadedData(data);
+        setTimeout(() => {
+          csvDownloadRef.current.link.click();
+        }, 500);
+      })
+      .catch((error) => alert("Error happened"));
+  };
+
+  const fetchData = () => {
+    axios
+      .get("http://localhost:4000/users/get")
+      .then(({ data }) => {
+        setData(data);
+      })
+      .catch((error) => alert("Error happened"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+    <div style={{ padding: "20px" }}>
+      <div>
+        <CSVLink data={data} headers={headers} filename="parents.csv">
+          <button className="btn btn-primary mb-2">Export</button>
+        </CSVLink>
+        <CSVLink
+          headers={headers}
+          data={downloadedData}
+          filename="parents.csv"
+          className="hidden"
+          ref={csvDownloadRef}
           target="_blank"
-          rel="noopener noreferrer"
+        />
+        <button
+          className="btn btn-primary mb-2"
+          onClick={fetchDataToDownload}
+          style={{ marginLeft: "5px" }}
         >
-          Learn React
-        </a>
-      </header>
+          Export Async
+        </button>
+      </div>
+      <table className="table table-bordered ">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <tr key={row?._id}>
+              <td>{row?.name}</td>
+              <td>{row?.age}</td>
+              <td>{row?.phone}</td>
+              <td>{row?.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
